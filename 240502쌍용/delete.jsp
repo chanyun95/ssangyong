e01217805882bcd2c4c7d7f1fbfc2048bfbfcd48
chanyun95 <167723808+chanyun95@users.noreply.github.com>
2024-05-02 12:55:35 +0900

    Add files via upload

---
+++ 240502쌍용/delete.jsp
@@ -0,0 +1,36 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ page import="kr.board.dao.BoardDAO" %>
+<%@ page import="kr.board.vo.BoardVO" %>
+<%
+	request.setCharacterEncoding("utf-8");
+
+	int num = Integer.parseInt(request.getParameter("num"));
+	String passwd = request.getParameter("passwd");
+
+	BoardDAO dao = BoardDAO.getInstance();
+	//비밀번호 인증을 위해 기본키를 전달해서 한 건의 레코드를 전달받음
+	BoardVO db_board = dao.getBoard(num);
+	
+	boolean check = false;
+	if(db_board!=null){
+		//비밀번호 체크
+		check = db_board.isCheckedPassword(passwd);
+	}
+	if(check){//인증 성공
+		dao.delete(num);//글 삭제
+%>
+	<script>
+		alert('글 삭제를 완료했습니다.');
+		location.replace('list.jsp');
+	</script>
+<%
+	}else{//인증 실패
+%>
+	<script>
+		alert('비밀번호 불일치');
+		history.go(-1);
+	</script>
+<%		
+	}
+%>
---
+++ 240502쌍용/kr.dao/BoardDAO.java
@@ -0,0 +1,190 @@
+package kr.board.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import kr.board.vo.BoardVO;
+import kr.util.DBUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BoardDAO {
+	private static BoardDAO instance = new BoardDAO();
+	
+	public static BoardDAO getInstance() {
+		return instance;
+	}
+	private BoardDAO() {}
+	
+	//글 저장
+	public void insert(BoardVO boardVO)throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = null;
+		try {
+			conn = DBUtil.getConnection();
+			
+			sql = "INSERT INTO mboard (num,title,name,passwd,content,ip) VALUES(mboard_seq.nextval,?,?,?,?,?)";
+			
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, boardVO.getTitle());
+			pstmt.setString(2, boardVO.getName());
+			pstmt.setString(3, boardVO.getPasswd());
+			pstmt.setString(4, boardVO.getContent());
+			pstmt.setString(5, boardVO.getIp());
+			
+			pstmt.executeUpdate();
+			
+		}catch(Exception e) {
+			throw new Exception(e);
+		}finally {
+			DBUtil.executeClose(null, pstmt, conn);
+		}
+	}
+	//글의 총 개수
+	public int getCount()throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		int count = 0;
+		try {
+			conn = DBUtil.getConnection();
+			
+			sql = "SELECT COUNT(*) FROM mboard";
+			
+			pstmt = conn.prepareStatement(sql);
+			
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				count = rs.getInt(1); // 1 > 컬럼 인덱스
+			}
+			
+		}catch(Exception e) {
+			throw new Exception(e);
+		}finally {
+			DBUtil.executeClose(rs, pstmt, conn);
+		}
+		return count;
+	}
+	//글 목록
+	public List<BoardVO> getList(int startRow,int endRow) throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		List<BoardVO> list = null;
+		String sql = null;
+		try {
+			conn = DBUtil.getConnection();
+			
+			sql = "SELECT * FROM (SELECT a.*, rownum rnum FROM (SELECT * FROM mboard ORDER BY num DESC)a) WHERE rnum>=? AND rnum<=?";
+			
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setInt(1, startRow);
+			pstmt.setInt(2, endRow);
+			
+			rs = pstmt.executeQuery();
+			list = new ArrayList<BoardVO>();
+			while(rs.next()) {
+				BoardVO boardVO = new BoardVO();
+				boardVO.setNum(rs.getInt("num"));
+				boardVO.setTitle(rs.getString("title"));
+				boardVO.setName(rs.getString("name"));
+				boardVO.setReg_date(rs.getDate("reg_date"));
+				//자바빈을 ArrayList에 저장
+				list.add(boardVO);
+			}
+		}catch(Exception e) {
+			throw new Exception(e);
+		}finally {
+			DBUtil.executeClose(rs, pstmt, conn);
+		}	
+		return list;
+	}
+	//글 상세
+	public BoardVO getBoard(int num)throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		BoardVO board = null;
+		String sql = null;
+		try {
+			conn = DBUtil.getConnection();
+			
+			sql = "SELECT * FROM mboard WHERE num=?";
+			
+			pstmt = conn.prepareCall(sql);
+			
+			pstmt.setInt(1, num);
+			
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				board = new BoardVO();
+				board.setNum(rs.getInt("num"));
+				board.setTitle(rs.getString("title"));
+				board.setName(rs.getString("name"));
+				board.setPasswd(rs.getString("passwd"));
+				board.setContent(rs.getString("content"));
+				board.setIp(rs.getString("ip"));
+				board.setReg_date(rs.getDate("reg_date"));
+			}
+		}catch(Exception e) {
+			throw new Exception(e);
+		}finally {
+			DBUtil.executeClose(rs, pstmt, conn);
+		}
+		return board;
+	}
+	//글 수정
+	public void update(BoardVO boardVO)throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql  = null;
+		try {
+			conn = DBUtil.getConnection();
+			
+			sql = "UPDATE mboard SET title=?,name=?,content=?,ip=? WHERE num=?";
+			
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, boardVO.getTitle());
+			pstmt.setString(2, boardVO.getName());
+			pstmt.setString(3, boardVO.getContent());
+			pstmt.setString(4, boardVO.getIp());
+			pstmt.setInt(5, boardVO.getNum());
+			
+			pstmt.executeUpdate();
+			
+		}catch(Exception e) {
+			throw new Exception(e);
+		}finally {
+			DBUtil.executeClose(null, pstmt, conn);
+		}
+	}
+	//글 삭제
+	public void delete(int num)throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = null;
+		try {
+			conn = DBUtil.getConnection();
+			
+			sql = "DELETE FROM mboard WHERE num=?";
+			
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setInt(1, num);
+			
+			pstmt.executeUpdate();
+			
+		}catch(Exception e) {
+			throw new Exception(e);
+		}finally {
+			DBUtil.executeClose(null, pstmt, conn);
+		}
+	}
+}
